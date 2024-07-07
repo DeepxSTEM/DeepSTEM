056897974578b85a7a6fffbe379657a1f0bde87c
deepxstem <deepxstem@gmail.com>
2024-07-07 13:27:16 -0400

    Updates

--- get_embedding_func.py
+++ get_embedding_func.py
@@ -1,27 +1,21 @@
 from langchain_community.embeddings import OllamaEmbeddings
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.embeddings import NeMoEmbeddings
-from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings
-
 
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Among the following costs free embeddings, HuggingFaceEmbeddings seems to perform best
+# Among the following costs free embeddings, HuggingFaceEmbeddings seems to perform better others
+# Add your prefer Embeddings model here as you need
 
 HFembeddings = HuggingFaceEmbeddings()
 Olmembeddings = OllamaEmbeddings(model="nomic-embed-text")
-Nvdembeddings = NVIDIAEmbeddings(model="NV-Embed-QA", embed_documents="passage")
 
 def get_embedding_function(emb: str):
     if emb == "hf":
         return HFembeddings
     elif emb == "ollama":
         return Olmembeddings
-    elif emb == "nvd":
-        return Nvdembeddings
     else:
-        return HFembeddings
+        return HFembeddings #default to HuggingFaceEmbeddings if no other embedding model is specified
