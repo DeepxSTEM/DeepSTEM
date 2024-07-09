27d6f8dac6368fea9761b592eb6dad24545fe7cf
deepxstem <deepxstem@gmail.com>
2024-07-09 13:06:41 -0400

    Updates

--- Hybrid_RAG.py
+++ Hybrid_RAG.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from langchain_google_genai import GoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
